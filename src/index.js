e35f82a1fe23b7bc1aa5548cdaf817c161d62647
minhyepark <minhyepark.dev@gmail.com>
2022-01-26 23:39:23 +0900

    Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
+// mutate state는 절대 쓰지 마라.
+
+const reducer = (state = [], action) => {
     switch (action.type) {
-        case "ADD":
-            return count + 1;
-        case "MINUS":
-            return count - 1;
+        case ADD_TODO:
+            return [];
+        case DELETE_TODO:
+            return [];
         default:
-            return count;
+            return state;
     }
 };
-const countStore = createStore(countModifier);
 
-const onChange = () => {
-    number.innerText = countStore.getState();
+const store = createStore(reducer);
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-countStore.subscribe(onChange);
-
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+form.addEventListener("submit", onSubmit);
